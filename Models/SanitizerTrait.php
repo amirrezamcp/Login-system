327608dd75112434cefe6ac82c5eaa198df72dce
amirrezamcp <yt.bigpotato@gmail.com>
2024-04-03 19:16:26 +0330

    create SanitizerTrait

---
+++ Models/SanitizerTrait.php
@@ -0,0 +1,20 @@
+<?php
+namespace Models;
+
+trait SanitizerTrait {
+    public static function sanitizeInput($input) {
+        if(is_array($input)) {
+            return array_map([self::class, 'sanitizeValue'], $input);
+        }else {
+            return self::sanitizeValue($input);
+        }
+    }
+
+    public static function sanitizeValue($value) {
+
+        $sanitizedValue = trim($value);
+        $sanitizedValue = filter_var($sanitizedValue, FILTER_SANITIZE_STRING);
+        $sanitizedValue = htmlspecialchars($sanitizedValue, ENT_QUOTES, 'UTF-8');
+        return $sanitizedValue;
+    }
+}
