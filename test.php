30ecb23fd9e13fe4099401e7329cd361fa2ef135
amirrezamcp <yt.bigpotato@gmail.com>
2024-04-03 19:16:34 +0330

    test SanitizerTrait

--- test.php
+++ test.php
@@ -3,14 +3,21 @@ require_once 'autoload.php';
 use Models\Database;
 
 class User extends Database {
+    use Models\SanitizerTrait;
     public function get($id) {
         $sql = "SELECT * FROM users WHERE id= ?";
         $stmt = $this->executeStatement($sql, $id);
         $result = $stmt->get_result();
         return $result->fetch_assoc();
     }
+
+    public function test($input) {
+        return Self::sanitizeInput($input);
+    }
 }
 
 $userTable = new User();
-$user = $userTable->get([1]);
+$input1 = "amr";
+$input2 = ['sads', 'saadasda', '456464'];
+$user = $userTable->test($input1);
 var_dump($user);
