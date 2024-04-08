d838e8767476b831e7b7cfa55ee75b8b82fa8108
amirrezamcp <yt.bigpotato@gmail.com>
2024-04-08 14:12:45 +0330

    Create token class and method

---
+++ Models/Token.php
@@ -0,0 +1,34 @@
+<?php
+namespace Models;
+use DateTime;
+
+class Token extends Database{
+    public function saveToken($user_id, $type) {
+        $token = bin2hex(random_bytes(32));
+        $expiration = (new DateTime())->modify('+1 day')->format('Y-m-d H:i:s');
+        $sql = "INSERT INTO tokens (user_id, token, type, expiration) VALUES (?, ?, ?, ?)";
+        $params = [
+            $user_id,
+            $token,
+            $type,
+            $expiration
+        ];
+        $stmt = $this->executeStatement($sql, $params);
+        if($stmt->affected_rows == 1) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function getToken($user_id, $type) {
+        $sql = "SELECT token FROM tokens WHERE user_id = ? AND type = ? AND expiration > NOW() AND used = '0' ORDER BY created_at DESC LIMIT 1";
+        $params = [
+            $user_id,
+            $type
+        ];
+        $stmt = $this->executeStatement($sql, $params);
+        $result = $stmt->get_result();
+        return $result->fetch_assoc();
+    }
+}
