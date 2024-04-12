76e548acd612516758fed304e1d764b630465e16
amirrezamcp <yt.bigpotato@gmail.com>
2024-04-12 10:56:07 +0330

    creat class Mail

---
+++ Models/Mail.php
@@ -0,0 +1,26 @@
+<?php
+namespace Models;
+
+class Mail {
+    private static $to;
+    private static $subject;
+    private static $message;
+    private static $headers;
+    private static $senderEmailAddress = 'mousavi.amirreza30@gmail.com';
+
+    public static function send($to, $subject, $message) {
+        self::$to = $to;
+        self::$subject = $subject;
+        self::$message = $message;
+        
+        self::$headers = "MIME-VERSION: 1.0" . "\r\n";
+        self::$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        self::$headers .= "From: " . self::$senderEmailAddress . "\r\n";
+
+        if(mail(self::$to, self::$subject, self::$message, self::$headers)) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
