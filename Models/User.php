577785170544d10e3bd8aed2838fa621e3c87a86
amirrezamcp <yt.bigpotato@gmail.com>
2024-04-20 20:08:15 +0330

    verify Email User

---
+++ Models/User.php
@@ -0,0 +1,29 @@
+<?php
+namespace Models;
+
+class User extends Database {
+    use SanitizerTrait;
+    public function getIdByEmail($email) {
+        $email = $this->sanitizeInput($email);
+        $sql = "SELECT id FROM users WHERE email = ?";
+        $params = [
+            $email
+        ];
+        $stmt = $this->executeStatement($sql, $params);
+        $result = $stmt->get_result();
+        return $result->fetch_assoc();
+    }
+    public function verifyEmail($id) {
+        $id = $this->sanitizeInput($id);
+        $sql = "UPDATE users SET is_email_verified = '1' WHERE id = ?";
+        $params = [
+            $id
+        ];
+        $stmt = $this->executeStatement($sql, $params);
+        if($stmt->affected_rows == 1) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
